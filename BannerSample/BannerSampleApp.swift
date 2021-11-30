58e0447cfd16a33600dd7a5954397d5e1a048155
Ryosuke Hiramatsu <himaratsu@gmail.com>
2021-11-30 15:50:03 +0900

    Initial Commit

---
+++ BannerSample/BannerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BannerSampleApp.swift
+//  BannerSample
+//
+//  Created by rh on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct BannerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
